1a791f0458067f5d0c0192a61a18f3def3447d9f
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-16 01:52:19 +0800

    modified main

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,18 @@
 <script>
-	export let name;
+	let firstName = 'Ivandi'
+	let lastName = 'Djoh Gah'
+
+	$: name = firstName + ' ' + lastName
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello, {name}!</h1>
+	<!-- 
+	{#if !age}
+		<p>I'm not <strong>{age -1}</strong> years old right now.</p>
+	{:else}
+		<p>I'm <strong>{age}</strong> years old right now.</p>
+	{/if} -->
 </main>
 
 <style>
@@ -16,7 +24,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: rgb(17, 15, 128);
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
