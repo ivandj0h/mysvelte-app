6b245e2616c93d4ae8a512af39dc38facf520d99
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-16 02:53:22 +0800

    components updated!

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,25 @@
 		{
 			id: 1,
 			rating: 5,
+			title: 'Lorem Ipsum 1',
 			text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
 		},
 		{
 			id: 2,
 			rating: 3,
+			title: 'Lorem Ipsum 2',
 			text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
 		},
 		{
 			id: 3,
 			rating: 1,
+			title: 'Lorem Ipsum 3',
 			text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
 		},
 	]
 </script>
 
-<main>
+<main class="container">
 	<FeedbackList {feedback} />
 </main>
 
--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -9,6 +9,30 @@
     </div>
     <div class="close">x</div>
     <div class="text-display">
+        <h3>{item.title}</h3>
         {item.text}
     </div>
 </Card>
+
+<style>
+    .num-display {
+        position: absolute;
+        top: -10px;
+        left: -10px;
+        width: 50px;
+        height: 50px;
+        background: #ff6a95;
+        color: #fff;
+        border: 1px #eee solid;
+        border-radius: 50%;
+        padding: 10px;
+        text-align: center;
+        font-size: 19px;
+    }
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        cursor: pointer;
+    }
+</style>
