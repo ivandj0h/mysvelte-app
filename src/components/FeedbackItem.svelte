038376b03fe2cc92af9c569cccd5d9ce5fbabb57
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-16 02:40:49 +0800

    components added

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Card from './Card.svelte'
+    export let item
+</script>
+
+<Card>
+    <div class="num-display">
+        {item.rating}
+    </div>
+    <div class="close">x</div>
+    <div class="text-display">
+        {item.text}
+    </div>
+</Card>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+    import FeedbackItem from "./FeedbackItem.svelte";
+    export let feedback = []
+</script>
+
+{#each feedback as fb (fb.id)}
+    <FeedbackItem item={fb} />
+{/each}
